b9ad60647981b0f3f110b2b95a373be2280bb09b
speedjunkie <speedjunkie@users.noreply.github.com>
2017-08-23 15:36:29 +0000

    Made Manager easier to unit test

--- src/Manager.php
+++ src/Manager.php
@@ -18,11 +18,10 @@ class Manager implements ManagerInterface {
 		$ret = $this->getIdentifier($autoloader);
 
 		if (!$this->has($ret)) {
-			$this->loaders[$ret] = $autoloader;
+			$this->setLoader($ret, $autoloader);
 
-			spl_autoload_register(
+			$this->wrapSplAutoloadRegister(
 				$this->getAutoloadFunction($autoloader),
-				true,
 				$prepend
 			);
 
@@ -30,27 +29,28 @@ class Manager implements ManagerInterface {
 		}
 
 		throw new Exception\AlreadyRegisteredException(
-			"Instance of AutoLoaderInterface already registered"
+			"Instance of AutoLoaderInterface already registered",
+			17
 		);
 	}
 
 	public function unregister(string $identifier): ManagerInterface {
 		if ($this->has($identifier)) {
-			$autoloader = $this->loaders[$identifier];
+			$autoloader = $this->getLoader($identifier);
 
-			unset($this->loaders[$identifier]);
+			$this->removeLoader($identifier);
 
-			spl_autoload_unregister(
+			$this->wrapSplAutoloadUnregister(
 				$this->getAutoloadFunction($autoloader)
 			);
 
 			return $this;
 		}
 
-		throw new Exception\InvalidIdentifierException(sprintf(
-			"Invalid identifier \"%s\".",
-			$identifier
-		));
+		throw new Exception\InvalidIdentifierException(
+			sprintf("Invalid identifier \"%s\".", $identifier),
+			2
+		);
 	}
 
 	/**
@@ -60,8 +60,21 @@ class Manager implements ManagerInterface {
 	 *
 	 * @return callable
 	 */
-	protected function getAutoloadFunction(AutoLoaderInterface $autoloader): callable {
+	protected function getAutoloadFunction(
+		AutoLoaderInterface $autoloader
+	): callable {
 		return [$autoloader, "load"];
+	}
+
+	/**
+	 * Get an instance of AutoLoaderInterface by identifier
+	 *
+	 * @param string $identifier
+	 *
+	 * @return \ResumeNext\AutoLoader\AutoLoaderInterface
+	 */
+	protected function getLoader(string $identifier): AutoLoaderInterface {
+		return $this->loaders[$identifier];
 	}
 
 	/**
@@ -73,6 +86,58 @@ class Manager implements ManagerInterface {
 	 */
 	protected function getIdentifier($object): string {
 		return spl_object_hash($object);
+	}
+
+	/**
+	 * Remove an instance of AutoLoaderInterface
+	 *
+	 * @param string $identifier
+	 *
+	 * @return void
+	 */
+	protected function removeLoader(string $identifier) {
+		unset($this->loaders[$identifier]);
+	}
+
+	/**
+	 * Set an instance of AutoLoaderInterface by identifier
+	 *
+	 * @param string                                     $identifier
+	 * @param \ResumeNext\AutoLoader\AutoLoaderInterface $loader
+	 *
+	 * @return void
+	 */
+	protected function setLoader(
+		string $identifier,
+		AutoLoaderInterface $loader
+	) {
+		$this->loaders[$identifier] = $loader;
+	}
+
+	/**
+	 * Wraps spl_autoload_register
+	 *
+	 * @param callable $autoload_function
+	 * @param bool     $prepend
+	 *
+	 * @return void
+	 */
+	protected function wrapSplAutoloadRegister(
+		callable $autoload_function,
+		bool $prepend = false
+	) {
+		spl_autoload_register($autoload_function, true, $prepend);
+	}
+
+	/**
+	 * Wraps spl_autoload_unregister
+	 *
+	 * @param callable $autoload_function
+	 *
+	 * @return void
+	 */
+	protected function wrapSplAutoloadUnregister(callable $autoload_function) {
+		spl_autoload_unregister($autoload_function);
 	}
 
 }
